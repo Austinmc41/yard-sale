bdfc7f09986295ebbec245ae905d5d55cf923392
Austinmc41 <austinmc41@gmail.com>
2022-01-07 21:39:55 -0500

    creating layout for basic yardsale app

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Card = () => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow'>
+        <img alt='sale item' src="/images/wacky_glasses.png?20"/>
+        <div>
+            <h2>John Doe</h2>
+            <p>jane.doe@gmail.com
+        </p>
+        </div>
+        </div>
+    )
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card/>
+    <Card/>
+    <Card/>
   </React.StrictMode>,
   document.getElementById('root')
 );
